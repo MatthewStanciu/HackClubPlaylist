fecaeadec4dadb02d511fada762de5332a1d741a
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-01 17:41:52 -0400

    got actual client secret in /refreshtoken

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/refresh_token', function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + client_secret).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
