669fd594b31efd2391359a1e2815758da7644d84
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-12 00:03:45 -0400

    add another failed attempt

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var request = require('request');
 var refresh = require('spotify-refresh');
+var Spotify = require('spotify-web-api-node');
 require('dotenv').config();
+
+var spotify = new Spotify({
+  accessToken: 'BQCSejhAJRE3QQVHfhfu-4XFrC317fRkMC0IRqPcVklhUU0SJfq-dpNRSkRKBeic7luZjkmLJcAUCgLVnR-PtwUZFFajmNEq_Dvnun8zVA2ik8at4nUxW1TEap24SAVgTATYNhoCVBQAnO82UvC2TkyfOYJifTiz7Ajh6SuwUIBNxmusdGd4xWTB5vNgflpAP5-NFu-Jhsctwu9hUVwWsLasGITSLbBPAA6nGo4xaA'
+});
+
+trackUri = "";
 
 function getIDfromUrl(url) {
   var split = url.split(/[?/]+/);
@@ -15,26 +22,40 @@ app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/song', function(req, res) {
-  refresh(process.env.REFRESH_TOKEN, process.env.CLIENT_ID, process.env.CLIENT_SECRET, function(err, res, body) {
-    if (err) return err;
-    request.post({
-      url: 'https://api.spotify.com/v1/playlists/5dPp7yV9i8mELe1Kk9UC6D/tracks?uris=spotify%3Atrack%3A' +
-        getIDfromUrl(req.body.submiturl),
-      headers: {
-        'Authorization': 'Bearer ' + body.access_token,
-        'Host': 'api.spotify.com',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      json: true
-    });
+function post(uri, access_token) {
+  request.post({
+    url: 'https://api.spotify.com/v1/playlists/5dPp7yV9i8mELe1Kk9UC6D/tracks?uris=spotify%3Atrack%3A' +
+      uri,
+    headers: {
+      'Authorization': 'Bearer ' + access_token,
+      'Host': 'api.spotify.com',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    json: true
   });
 
-  console.log("added " + req.body.submiturl + " to the playlist");
-  res.redirect("/added");
+  console.log("uri: " + uri);
+}
+
+app.post('/song', function(req, postRes) {
+  /*refresh(process.env.REFRESH_TOKEN, process.env.CLIENT_ID, process.env.CLIENT_SECRET, function(err, res, body) {
+    console.log(body.access_token);
+    spotify.setAccessToken(body.access_token);
+  })*/
+  var formatted = (req.body.submiturl).replace(" ", "%20");
+  var formattedArtist = (req.body.submitartist).replace(" ", "%20")
+  spotify.searchTracks('track:Alright artist:Kendrick Lamar')
+  .then(function(data) {
+    console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
 })
 
+app.get('/add', function(req, res) {
+  console.log(trackUri);
+})
 
 app.get("/", function(err, res) {
   res.sendFile(__dirname + "/index.html");
