260a94ff162b6d0292efdbc110f59ce6b55e11c3
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-12 21:18:09 -0400

    add search feature

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ var refresh = require('spotify-refresh');
 var Spotify = require('spotify-web-api-node');
 require('dotenv').config();
 
-var spotify = new Spotify({
-  accessToken: 'BQCSejhAJRE3QQVHfhfu-4XFrC317fRkMC0IRqPcVklhUU0SJfq-dpNRSkRKBeic7luZjkmLJcAUCgLVnR-PtwUZFFajmNEq_Dvnun8zVA2ik8at4nUxW1TEap24SAVgTATYNhoCVBQAnO82UvC2TkyfOYJifTiz7Ajh6SuwUIBNxmusdGd4xWTB5vNgflpAP5-NFu-Jhsctwu9hUVwWsLasGITSLbBPAA6nGo4xaA'
-});
+var spotify = new Spotify();
+spotify.setRefreshToken(process.env.REFRESH_TOKEN);
+spotify.setClientId(process.env.CLIENT_ID);
+spotify.setClientSecret(process.env.CLIENT_SECRET);
 
 trackUri = "";
 
@@ -17,48 +18,50 @@ function getIDfromUrl(url) {
   var split = url.split(/[?/]+/);
   return split[3];
 }
+function getIDfromUri(uri) {
+  var split = uri.split(/[:\"]+/);
+  return split[3];
+}
 
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function post(uri, access_token) {
-  request.post({
-    url: 'https://api.spotify.com/v1/playlists/5dPp7yV9i8mELe1Kk9UC6D/tracks?uris=spotify%3Atrack%3A' +
-      uri,
-    headers: {
-      'Authorization': 'Bearer ' + access_token,
-      'Host': 'api.spotify.com',
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    json: true
-  });
-
-  console.log("uri: " + uri);
-}
-
-app.post('/song', function(req, postRes) {
-  /*refresh(process.env.REFRESH_TOKEN, process.env.CLIENT_ID, process.env.CLIENT_SECRET, function(err, res, body) {
-    console.log(body.access_token);
-    spotify.setAccessToken(body.access_token);
-  })*/
-  var formatted = (req.body.submiturl).replace(" ", "%20");
-  var formattedArtist = (req.body.submitartist).replace(" ", "%20")
-  spotify.searchTracks('track:Alright artist:Kendrick Lamar')
+app.post('/song', function(req, res) {
+  var track = (req.body.submiturl);
+  var artist = (req.body.submitartist);
+  console.log(spotify.getAccessToken());
+  spotify.searchTracks('track:'+track+ ' artist:'+ artist)
   .then(function(data) {
-    console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
+    var uri = JSON.stringify(data.body['tracks']['items'][0]['uri']);
+    console.log(uri);
+      request.post({
+        url: 'https://api.spotify.com/v1/playlists/5dPp7yV9i8mELe1Kk9UC6D/tracks?uris=spotify%3Atrack%3A' +
+          getIDfromUri(uri),
+        headers: {
+          'Authorization': 'Bearer ' + spotify.getAccessToken(),
+          'Host': 'api.spotify.com',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Content-Length': 0
+        },
+        json: true
+      });
   }, function(err) {
-    console.log('Something went wrong!', err);
-  });
-})
+    console.log('Something wong!', err);
+  })
 
-app.get('/add', function(req, res) {
-  console.log(trackUri);
+  res.redirect('/added');
 })
 
 app.get("/", function(err, res) {
   res.sendFile(__dirname + "/index.html");
+  spotify.refreshAccessToken().then(function(data) {
+    spotify.setAccessToken(data.body['access_token']);
+  },function(err) {
+    console.log('Could not refresh access token', err);
+    }
+  );
 })
 app.get("/added", function(err, res) {
   res.sendFile(__dirname + "/added.html");
