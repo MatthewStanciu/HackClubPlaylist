35b2bd1b8ff9c6077b17d70ef5571d28ecdb4e25
MatthewStanciu <mattbstanciu@gmail.com>
2018-10-20 21:50:45 -0400

    format code like schacks music

--- server.js
+++ server.js
@@ -1,94 +1,86 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var bodyParser = require('body-parser');
-var request = require('request');
-var refresh = require('spotify-refresh');
-var Spotify = require('spotify-web-api-node');
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
 
-var spotify = new Spotify();
-spotify.setRefreshToken(process.env.REFRESH_TOKEN);
-spotify.setClientId(process.env.CLIENT_ID);
-spotify.setClientSecret(process.env.CLIENT_SECRET);
+const bodyParser = require('body-parser')
+const request = require('request')
+const refresh = require('spotify-refresh')
+const Spotify = require('spotify-web-api-node')
+require('dotenv').config()
+
+const spotify = new Spotify()
+spotify.setRefreshToken(process.env.REFRESH_TOKEN)
+spotify.setClientId(process.env.CLIENT_ID)
+spotify.setClientSecret(process.env.CLIENT_SECRET)
 
 function getIDfromUri(uri) {
-  var split = uri.split(/[:\"]+/);
-  return split[3];
+  const split = uri.split(/[:\"]+/)
+  return split[3]
 }
 
-function post(uri) {
+const post = (uri) =>
   request.post({
-    url: 'https://api.spotify.com/v1/playlists/5dPp7yV9i8mELe1Kk9UC6D/tracks?uris=spotify%3Atrack%3A' +
-      getIDfromUri(uri),
+    url: `https://api.spotify.com/v1/playlists/1GFcGQfzTuluKXn6chPi8O/tracks?uris=spotify%3Atrack%3A${getIDfromUri(uri)}`,
     headers: {
-      'Authorization': 'Bearer ' + spotify.getAccessToken(),
-      'Host': 'api.spotify.com',
-      'Accept': 'application/json',
+      Authorization: `Bearer ${spotify.getAccessToken()}`,
+      Host: 'api.spotify.com',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
       'Content-Length': 0
     },
     json: true
-  });
-}
+  })
 
-app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use('/public', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.post('/song', function(req, res) {
-  var track = (req.body.submiturl);
-  var artist = (req.body.submitartist);
+app.post('/song', (req, res) => {
+  const track = req.body.submiturl
+  const artist = req.body.submitartist
 
-  if (artist != "") {
-    spotify.searchTracks('track:'+track+ ' artist:'+ artist)
-    .then(function(data) {
-      var uri = JSON.stringify(data.body['tracks']['items'][0]['uri']);
-      var isExplicit = JSON.stringify(data.body['tracks']['items'][0]['explicit']);
-      if (isExplicit === "true") {
-        console.log("requested song " + track + " not added because it's explicit");
-        return res.sendFile(__dirname + '/index.html');
+  if (artist != '') {
+    spotify.searchTracks(`track:${track} artist:${artist}`).then(
+      data => {
+        const uri = JSON.stringify(data.body['tracks']['items'][0]['uri'])
+        post(uri)
+        console.log(`added ${track} by ${artist} to the playlist!`)
+      },
+      err => {
+        console.log(err)
       }
-      else {
-        post(uri);
-        console.log("added " + track + " by " + artist +" to the playlist!");
-        return res.sendFile(__dirname + '/added.html');
-      }
-    }, function(err) {
-      console.log(err);
-    })
+    )
   } else {
-    spotify.searchTracks(track)
-    .then(function(data) {
-      var uri = JSON.stringify(data.body['tracks']['items'][0]['uri']);
-      var isExplicit = JSON.stringify(data.body['tracks']['items'][0]['explicit']);
-      if (isExplicit === "true") {
-        console.log("requested song " + track + " not added because it's explicit");
-        return res.sendFile(__dirname + '/index.html');
+    spotify.searchTracks(track).then(
+      data => {
+        const uri = JSON.stringify(data.body['tracks']['items'][0]['uri'])
+        post(uri)
+        console.log(`added ${track} to the playlist!`)
+      },
+      err => {
+        console.log(err)
       }
-      else {
-        post(uri);
-        console.log("added " + track + " by " + artist +" to the playlist!");
-        return res.sendFile(__dirname + '/added.html');
-      }
-    }, function(err) {
-      console.log(err);
-    })
+    )
   }
+
+  res.redirect('/added')
 })
 
-app.get("/", function(err, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (err, res) => {
+  res.sendFile(`${__dirname}/index.html`)
 
-  spotify.refreshAccessToken().then(function(data) {
-    spotify.setAccessToken(data.body['access_token']);
-  },function(err) {
-    console.log('Could not refresh access token', err);
+  spotify.refreshAccessToken().then(
+    data => {
+      spotify.setAccessToken(data.body['access_token'])
+    },
+    err => {
+      console.log('Could not refresh access token', err)
     }
-  );
-})
-app.get("/added", function(err, res) {
-  res.sendFile(__dirname + "/added.html");
+  )
 })
 
-http.listen(3000);
+app.get('/added', (err, res) => {
+  res.sendFile(`${__dirname}/added.html`)
+})
+
+http.listen(3000)
